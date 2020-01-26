b916d10112d84a771a179b97e5254af02660cc36
Ben Jones <bljones36@gmail.com>
2020-01-26 18:44:34 -0500

    Added all resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :favorites
+  resources :ratings
+  resources :appointments
+  resources :stores
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
