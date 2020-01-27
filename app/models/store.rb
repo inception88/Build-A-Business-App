3fa8dc80070c0a116d19f41c75c55f4c1d840f02
Ben Jones <bljones36@gmail.com>
2020-01-26 19:43:33 -0500

    Added model relationships.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,7 @@
 class Store < ApplicationRecord
+    has_many :ratings
+    has_many :appointments
+    has_many :favorites
+    has_many :users, through: :favorites
+    has_many :users, through: :appointments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :ratings
+    has_many :appointments
+    has_many :favorites
+    has_many :stores, through: :favorites
+    has_many :stores, through: :appointments
+
 end
