f95b4db7da37469e5b0635a6ff6362f81358a866
Ben Jones <bljones36@gmail.com>
2020-01-29 21:43:07 -0500

    Updated validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true
     validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
     has_many :ratings
     has_many :appointments
