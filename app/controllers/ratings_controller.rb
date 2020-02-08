9b5ba834c5657d0a5866770c8e8ba1c5e846ed46
Ben Jones <bljones36@gmail.com>
2020-02-08 17:42:14 -0500

    Added ability to rate stores

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,19 @@
 class RatingsController < ApplicationController
+
+    def create
+        @rating = Rating.find_or_create_by(store_id: params[:id], user_id: current_user.id)
+        @rating.update(score: params[:score])
+        redirect_to store_path(@rating.store)
+    end
+
+    def destroy
+        @ratings = current_user.ratings
+        if @ratings.include?(Rating.find_by_id(params[:id]))
+            @rating = Rating.find_by_id(params[:id])
+            @store = @rating.store
+            @rating.destroy
+        end
+        redirect_to store_path(@store)
+    end
+
 end
--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -1,2 +1,19 @@
 module StoresHelper
+
+    def rated?(store)
+        if Rating.find_by(store_id: store.id, user_id: current_user.id)
+            true
+        else
+            false
+        end
+    end
+
+    def rating_score(store)
+       @rating = Rating.find_by(store_id: store.id, user_id: current_user.id)
+       @rating.score
+    end
+
+    def rating(store)
+        @rating = Rating.find_by(store_id: store.id, user_id: current_user.id)
+     end
 end
