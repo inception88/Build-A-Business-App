4cea7acd29c3186572ba2be33bb8fc79a6ba4fad
Ben Jones <bljones36@gmail.com>
2020-02-08 14:21:13 -0500

    Updated Favorites show page and controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,16 @@
 class FavoritesController < ApplicationController
+    def create
+        @favorite = Favorite.find_or_create_by(store_id: params[:id], user_id: current_user.id)
+        redirect_to favorite_path(@favorite)
+    end
+    
+    def show
+        @favorite = Favorite.find(params[:id])
+    end
+
+    private
+     
+    def favorite_params
+        params.require(:favorite).permit(:name, :password)
+    end
 end
