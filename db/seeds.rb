03dcfe6ace3045e3783fbed9955f1a047973c7ef
Ben Jones <bljones36@gmail.com>
2020-02-08 11:02:10 -0500

    Added show page for stores

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Store.create(name: "Toys-We-R", city: "Cincinnati", state: "Ohio")
