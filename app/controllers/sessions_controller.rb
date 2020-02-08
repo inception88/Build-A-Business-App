e2749a576a6f4b134b34a85f63d692407ffda187
Ben Jones <bljones36@gmail.com>
2020-02-08 09:18:37 -0500

    Fixed Session Login process

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
     def new
-        @user = User.new
     end
     
     def create
-        @user = User.find_by(email: params[:user_name])
+        @user = User.find_by(email: params[:email])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
