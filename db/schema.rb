af9f43b0c2b3408a0008520806a1a984f390f863
Ben Jones <bljones36@gmail.com>
2020-02-09 15:31:57 -0500

    Added omniauth params to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_012517) do
+ActiveRecord::Schema.define(version: 2020_02_09_202917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 2020_01_30_012517) do
     t.string "city"
     t.string "state"
     t.boolean "admin", default: false
+    t.string "provider"
+    t.string "uid"
+    t.text "image"
   end
 
 end
