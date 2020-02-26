e79bd4ef9b7f7d27f861d71a90d70a6288666d4f
inception88 <bljones36@gmail.com>
2020-02-25 20:46:09 -0500

    Added capability to add category to favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,12 @@ class FavoritesController < ApplicationController
     def show
     end
 
+    def update
+        @favorite = Favorite.find(params[:id])
+        @favorite.update(category: params[:favorite][:category])
+        redirect_to favorite_path(@favorite)
+    end
+
     def create
         @favorite = Favorite.find_or_create_by(store_id: params[:id], user_id: current_user.id)
         redirect_to favorite_path(@favorite)
